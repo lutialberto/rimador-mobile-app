756e2ec9905631ca501448815adc95f145bf4105
Alberto Luti <alberto_luti@edsa.com.ar>
2022-10-08 17:51:37 -0300

    Refactor. Added support to load more items to hook

--- src/features/swipeLeftOrRight/screens/swipe/hooks/useListSelector.ts
+++ src/features/swipeLeftOrRight/screens/swipe/hooks/useListSelector.ts
@@ -3,8 +3,17 @@ import {useState} from 'react';
 type REFRESH_MODES = 'keepFirst' | 'keepSecond' | 'keepNone';
 
 export const useListSelector = <T>() => {
-  const [listLoaded, setListLoaded] = useState<T[]>([]);
-  const [selectedItemsIndex, setSelectedItemsIndex] = useState<number[]>([]);
+  const [state, setState] = useState<{
+    listLoaded: T[];
+    firstSelectedIndex: number;
+    secondSelectedIndex: number;
+    needsReload: boolean;
+  }>({
+    listLoaded: [],
+    firstSelectedIndex: -1,
+    secondSelectedIndex: -1,
+    needsReload: false,
+  });
 
   const isNextRefreshPosible = (mode: REFRESH_MODES) => {
     const maxcurrentIndex = getMaxSelectedIndex();
@@ -13,21 +22,25 @@ export const useListSelector = <T>() => {
       mode === 'keepFirst' || mode === 'keepSecond' ? 1 : 2;
     const maxIndexOnNextRefresh =
       maxcurrentIndex + minimunItemsNeededForNextRefresh;
-    return listLoaded.length - 1 >= maxIndexOnNextRefresh;
+    return state.listLoaded.length - 1 >= maxIndexOnNextRefresh;
   };
 
   const getMaxSelectedIndex = () =>
-    Math.max(selectedItemsIndex[0], selectedItemsIndex[1]);
+    Math.max(state.firstSelectedIndex, state.secondSelectedIndex);
 
   const getSelectedItem = (option: 'first' | 'second') => {
     const leftOptionIndex =
-      option === 'first' ? selectedItemsIndex[0] : selectedItemsIndex[1];
-    return listLoaded[leftOptionIndex];
+      option === 'first' ? state.firstSelectedIndex : state.secondSelectedIndex;
+    return state.listLoaded[leftOptionIndex];
   };
 
-  const saveLoadedList = (options: T[]) => {
-    setListLoaded(options);
-    setSelectedItemsIndex([0, 1]);
+  const saveLoadedList = (listLoaded: T[]) => {
+    setState({
+      listLoaded,
+      firstSelectedIndex: 0,
+      secondSelectedIndex: 1,
+      needsReload: listLoaded.length < 2,
+    });
   };
 
   const changeNewSelectedItems = (mode: REFRESH_MODES): boolean => {
@@ -35,8 +48,8 @@ export const useListSelector = <T>() => {
 
     const maxCurrentIndex = getMaxSelectedIndex();
 
-    let firstItem = selectedItemsIndex[0];
-    let secondItem = selectedItemsIndex[1];
+    let firstItem = state.firstSelectedIndex;
+    let secondItem = state.secondSelectedIndex;
 
     switch (mode) {
       case 'keepFirst':
@@ -51,13 +64,46 @@ export const useListSelector = <T>() => {
         break;
     }
 
-    setSelectedItemsIndex([firstItem, secondItem]);
+    setState(prev => {
+      return {
+        ...prev,
+        firstSelectedIndex: firstItem,
+        secondSelectedIndex: secondItem,
+        needsReload:
+          prev.listLoaded.length - 2 < Math.max(firstItem, secondItem),
+      };
+    });
     return true;
+  };
+
+  const addMoreItems = (newItems: T[]) => {
+    const maxIndex = getMaxSelectedIndex();
+
+    const remaining =
+      maxIndex === state.listLoaded.length - 1
+        ? []
+        : state.listLoaded.slice(maxIndex + 1);
+
+    const newLoadedList = [
+      state.listLoaded[state.firstSelectedIndex],
+      state.listLoaded[state.secondSelectedIndex],
+      ...remaining,
+      ...newItems,
+    ];
+
+    setState({
+      listLoaded: newLoadedList,
+      firstSelectedIndex: 0,
+      secondSelectedIndex: 1,
+      needsReload: remaining.length + newItems.length < 2,
+    });
   };
 
   return {
     saveLoadedList,
     getSelectedItem,
     changeNewSelectedItems,
+    addMoreItems,
+    needsReload: state.needsReload,
   };
 };
